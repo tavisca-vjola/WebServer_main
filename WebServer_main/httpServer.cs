fae0c65b73127a98a94e1da3fdc01b8c166cc6e8
vjola <vjola@tavisca.com>
2019-08-02 14:19:37 +0530

    Add project files.

---
+++ WebServer_main/httpServer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebServer_main
+{
+    class httpServer
+    {
+        private TcpListener listener;
+        bool running = false;
+        httpServer(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+        }
+        public void start()
+        {
+            Thread server = new Thread(new ThreadStart(Run));
+            server.Start();
+        }
+
+       public void Run()
+        {
+            running = true;
+            listener.Start();
+            while(running)
+            {
+                TcpClient client = listener.AcceptTcpClient();
+                HandleClient(client);
+                client.Close();
+
+
+
+            }
+            running = false;
+            listener.Stop();
+
+        }
+
+        private void HandleClient(TcpClient client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
